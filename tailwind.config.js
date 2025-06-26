0a54e8f4a650a6e8d1e29463bbc3c1ab4bf109b7
femidaniel98 <femidanielofficial@gmail.com>
2025-06-26 14:36:45 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#039935",
+        secondary: "#090F09",
+      },
+    },
+  },
+  plugins: [],
+};
